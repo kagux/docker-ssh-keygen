5e7438540a89dbcf3b991b8e1f0b4ac10877a161
Boris Mikhaylov <kaguxmail@gmail.com>
2016-04-20 17:58:38 +0200

    configurable owner

--- keygen.sh
+++ keygen.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-KEYS_PATH=${1:-/root/.ssh}
+KEYS_PATH=${KEYS_PATH:-/root/.ssh}
 PRIVATE_KEY=$KEYS_PATH/id_rsa
 PUBLIC_KEY=${PRIVATE_KEY}.pub
+UID=${UID:-0}
+GUI=${GUI:-0}
 
 if [ ! -f "$PRIVATE_KEY" ]; then
   /usr/bin/ssh-keygen -q -t rsa -N '' -f $PRIVATE_KEY
   chmod 700 $KEYS_PATH
   chmod 644 $PUBLIC_KEY
   chmod 600 $PRIVATE_KEY
+  chown $UID:$GID $KEYS_PATH
 fi
 
 echo "========= PUBLIC KEY ============"
